f6650e958a1b217aa23c62b919a4cc19f6a1ff38
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-09-03 19:09:29 +0530

    Created Dashboard Module

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,19 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import React from "react";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  return <HomeView />;
+};
+
+export default Page;
