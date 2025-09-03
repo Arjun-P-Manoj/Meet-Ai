0b6de1f7888f4aa64c5d3a761fe28342483fc8d6
Arjun-P-Manoj <arjunpmanoj1@gmail.com>
2025-09-03 19:11:48 +0530

    fix gradient colours in signup view

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -222,7 +222,7 @@ export const SignUpView = () => {
               </div>
             </form>
           </Form>
-          <div className="bg-gradient-to-r from-green-700 to-green-900 relative hidden md:flex md:flex-col gap-y-4 items-center justify-center">
+          <div className="bg-gradient-to-r from-sidebar-accent to-sidebar relative hidden md:flex md:flex-col gap-y-4 items-center justify-center">
             <img className="h-[92px] w-[92px]" src="/logo.svg" alt="Image" />
             <p className="text-2xl font-semibold text-white">Meet.AI</p>
           </div>
